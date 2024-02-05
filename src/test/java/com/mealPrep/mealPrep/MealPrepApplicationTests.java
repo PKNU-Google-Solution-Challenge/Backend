0924687e7459ebed2cbc983d104766de4abd9543
Haram0111 <5gkfka5@naver.com>
2024-02-06 01:40:47 +0900

    first commmit

---
+++ src/test/java/com/mealPrep/mealPrep/MealPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealPrep.mealPrep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
