e43b7697e7b54092909c5a770a959487ed561f11
최민석 <cms8955@naver.com>
2024-02-11 19:22:30 +0900

    feat: Rename Controller

--- src/main/java/com/mealPrep/mealPrep/controller/RecipeController.java
+++ src/main/java/com/mealPrep/mealPrep/controller/RecipeController.java
@@ -1,4 +1,4 @@
-package com.mealPrep.mealPrep.Controller;
+package com.mealPrep.mealPrep.controller;
 
 import com.mealPrep.mealPrep.common.Response;
 import io.swagger.v3.oas.annotations.Operation;
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
