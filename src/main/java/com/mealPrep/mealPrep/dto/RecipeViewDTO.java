7b6ef1427dac717b4705cf3e08d23c1e0f5f2dc5
최민석 <cms8955@naver.com>
2024-02-12 15:44:39 +0900

    feat:Imgae Null처리

---
+++ src/main/java/com/mealPrep/mealPrep/dto/RecipeViewDTO.java
@@ -0,0 +1,26 @@
+package com.mealPrep.mealPrep.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mealPrep.mealPrep.domain.Enum.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class RecipeViewDTO {
+    private Long boardId;
+    private String author;
+    private String title;
+    private String body;
+    private String imgUrl;
+    private Long views;
+    private Long likes;
+    private Long totalTime;
+    private Long totalKcal;
+    private Long totalPrice;
+    private Category category;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern="yyyy-MM-dd")
+    private LocalDateTime createdAt;
+}
--- src/main/java/com/mealPrep/mealPrep/repository/RecipeRepository.java
+++ src/main/java/com/mealPrep/mealPrep/repository/RecipeRepository.java
@@ -4,8 +4,11 @@ import com.mealPrep.mealPrep.domain.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe,Long> {
 
+    List<Recipe> findAllByOrderByCreatedAtDesc();
 }
 
