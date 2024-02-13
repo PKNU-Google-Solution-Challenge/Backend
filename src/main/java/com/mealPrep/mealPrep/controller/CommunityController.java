3b1e82d44c39cd911b6fdd2e184bfd3ac6865542
Haram0111 <5gkfka5@naver.com>
2024-02-13 11:50:25 +0900

    jar error

---
+++ src/main/java/com/mealPrep/mealPrep/controller/CommunityController.java
@@ -0,0 +1,18 @@
+package com.mealPrep.mealPrep.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class CommunityController {
+
+//    @PostMapping("/community/write")
+//    public ResponseEntity write(){
+//        return new ResponseEntity(200);
+//    }
+}
