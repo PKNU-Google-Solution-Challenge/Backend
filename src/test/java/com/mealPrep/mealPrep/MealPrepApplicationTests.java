17327367937281cc4c75a851570a785d8fb56a53
최민석 <cms8955@naver.com>
2024-02-19 11:17:21 +0900

    feat: HTTPS 적용, 댓글 및 게시글 작성시 리워드 추가

--- src/test/java/com/mealPrep/mealPrep/MealPrepApplicationTests.java
+++ src/test/java/com/mealPrep/mealPrep/MealPrepApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MealPrepApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
